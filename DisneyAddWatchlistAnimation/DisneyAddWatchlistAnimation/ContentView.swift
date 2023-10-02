44d8d66a5def23626f4fa662ff50465c9e56a5a7
Anup DSouza <anup.dsouza@gmail.com>
2023-10-02 12:41:59 +0530

    Add plus sign animation

--- DisneyAddWatchlistAnimation/DisneyAddWatchlistAnimation/ContentView.swift
+++ DisneyAddWatchlistAnimation/DisneyAddWatchlistAnimation/ContentView.swift
@@ -7,13 +7,47 @@
 
 import SwiftUI
 
+struct AddWatchlist: Shape {
+    var animatableData: CGFloat {
+        get { return progress }
+        set { progress = newValue }
+    }
+    
+    var progress: CGFloat = 0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX + (rect.maxX * progress), y: rect.midY))
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY + (rect.maxY * progress)))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
+    @State private var drawProgress: CGFloat = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: 5)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.red)
+                .overlay {
+                    AddWatchlist(progress: drawProgress)
+                        .stroke(.black, lineWidth: 10.0)
+                        .frame(width: 200, height: 200)
+                }
+        }
+        .onAppear {
+            withAnimation(.linear(duration: 2.0)) {
+                drawProgress = 1.0
+            }
         }
         .padding()
     }
