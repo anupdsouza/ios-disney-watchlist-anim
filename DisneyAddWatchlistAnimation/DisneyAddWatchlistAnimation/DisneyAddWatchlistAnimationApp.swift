ffa54674f5c9699cec73da054adf9956e75a43f6
Anup DSouza <anup.dsouza@gmail.com>
2023-10-02 12:22:36 +0530

    Add initial project

---
+++ DisneyAddWatchlistAnimation/DisneyAddWatchlistAnimation/DisneyAddWatchlistAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyAddWatchlistAnimationApp.swift
+//  DisneyAddWatchlistAnimation
+//
+//  Created by Anup D'Souza on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DisneyAddWatchlistAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
